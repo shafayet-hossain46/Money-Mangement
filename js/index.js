2af092ee4762b52ba2d139bd5966efaf38eeb90a
shafayet-hossain46 <shafayethossaing46@gmail.com>
2022-06-19 14:52:57 +0600

    written a funciton for all

--- js/index.js
+++ js/index.js
@@ -1,29 +1,42 @@
 // Calculate Button
 document.getElementById("calculateButton").addEventListener("click", function(){
-    
+
     // Getting all expenses fields values.
     // Food Field
-    const foodField = document.getElementById("foodField");
-    const foodFieldText = foodField.value;
-    const foodFieldAmount = parseFloat(foodFieldText);
-    foodField.value = "";
-
+    // const foodField = document.getElementById("foodField");
+    // const foodFieldText = foodField.value;
+    // const foodFieldAmount = parseFloat(foodFieldText);
+    // foodField.value = "";
 
     // Rent Field
-    const rentField = document.getElementById("rentField");
-    const rentFieldText = rentField.value;
-    const rentFieldAmount = parseFloat(rentFieldText);
-    rentField.value = "";
+    // const rentField = document.getElementById("rentField");
+    // const rentFieldText = rentField.value;
+    // const rentFieldAmount = parseFloat(rentFieldText);
+    // rentField.value = "";
 
     // Cloths Field
-    const clothsField = document.getElementById("clothsField");
-    const clothsFieldText = clothsField.value;
-    const clothsFieldAmount = parseFloat(clothsFieldText);
-    clothsField.value = "";
+    // const clothsField = document.getElementById("clothsField");
+    // const clothsFieldText = clothsField.value;
+    // const clothsFieldAmount = parseFloat(clothsFieldText);
+    // clothsField.value = "";
 
+
+    // Function For ALL Field's Value.
+    function getAllFieldValue(inputFieldId){
+        const inputField = document.getElementById(inputFieldId);
+        const inputFieldText = inputField.value;
+        const expenseAmount = parseFloat(inputFieldText);
+        inputField.value = "";
+        return expenseAmount;
+    }
+
+    // Calling Function And Giving The ids To Get The Value.
+    const foodAmount = getAllFieldValue("foodField");
+    const rentAmount = getAllFieldValue("rentField");
+    const clothsAmount = getAllFieldValue("clothsField");
 
     // Getting The Total Of Food, Rent, Cloths.
-    const totalOfAll = foodFieldAmount + rentFieldAmount + clothsFieldAmount;
+    const totalOfAll = foodAmount + rentAmount + clothsAmount;
 
     // Total Expenses Field
     const totalExpensesField = document.getElementById("totalExpense");
