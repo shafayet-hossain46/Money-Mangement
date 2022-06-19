710b88b23ca808effd4c998cd7032fd0537d5f4f
shafayet-hossain46 <shafayethossaing46@gmail.com>
2022-06-19 16:10:06 +0600

    done

--- js/index.js
+++ js/index.js
@@ -1,32 +1,16 @@
 // Calculate Button
 document.getElementById("calculateButton").addEventListener("click", function(){
 
-    // Getting all expenses fields values.
-    // Food Field
-    // const foodField = document.getElementById("foodField");
-    // const foodFieldText = foodField.value;
-    // const foodFieldAmount = parseFloat(foodFieldText);
-    // foodField.value = "";
-
-    // Rent Field
-    // const rentField = document.getElementById("rentField");
-    // const rentFieldText = rentField.value;
-    // const rentFieldAmount = parseFloat(rentFieldText);
-    // rentField.value = "";
-
-    // Cloths Field
-    // const clothsField = document.getElementById("clothsField");
-    // const clothsFieldText = clothsField.value;
-    // const clothsFieldAmount = parseFloat(clothsFieldText);
-    // clothsField.value = "";
-
-
     // Function For ALL Field's Value.
     function getAllFieldValue(inputFieldId){
         const inputField = document.getElementById(inputFieldId);
         const inputFieldText = inputField.value;
         const expenseAmount = parseFloat(inputFieldText);
-        inputField.value = "";
+
+        // Condition For Not To Clear The Income Field
+        if(inputFieldId != "incomeField"){
+            inputField.value = "";
+        }
         return expenseAmount;
     }
 
@@ -41,4 +25,12 @@ document.getElementById("calculateButton").addEventListener("click", function(){
     // Total Expenses Field
     const totalExpensesField = document.getElementById("totalExpense");
     totalExpensesField.innerText = totalOfAll;
+
+    // Income 
+    const incomeAmount = getAllFieldValue("incomeField");
+    const restIncomeAmount = incomeAmount - totalOfAll;
+    
+    // Setting Balance Field
+    const balanceField = document.getElementById("balanceField");
+    balanceField.innerText = restIncomeAmount;
 })
